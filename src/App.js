3ee2ac1199092e32928d756bd737f6a3fc8e4aa3
Nam Minjeong <nmj5012@naver.com>
2021-06-23 17:10:58 +0900

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Router from './Component/Router';
 
 
 function App() {
   return (
-    <div className="App"/>
+    <Router/>
   );
 }
 
---
+++ src/Component/Router.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Main from "../Routes/Main";
+import Form from "../Routes/Form";
+import Submit from "../Routes/Submit";
+
+export default () => (
+    <Router>
+        <Header />
+        <Switch>
+            <Route exact path="/" exact component={Main} />
+            <Route path="/form" component={Form} />
+            <Route path="/submit" component={Submit} />
+            <Redirect path="*" to="/" />
+        </Switch>
+        <Footer />
+    </Router>
+);
