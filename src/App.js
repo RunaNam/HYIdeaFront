d39507eea2f7eb26fa8d613fceb22d0ee4ffd4a7
RunaNam <nmj5012@naver.com>
2021-06-24 16:42:11 +0900

    [feat] calendar 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Router from './Component/Router';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 
 function App() {
   return (
-    <Router/>
+    <Router />
   );
 }
 
