58286e76c8714958fb7aabd6128b29a4579670d4
Nam Minjeong <nmj5012@naver.com>
2021-06-27 18:11:53 +0900

    form calendar (미완)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Router from './Component/Router';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 
 function App() {
--- src/Routes/MyPage.js
+++ src/Routes/MyPage.js
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components";
+
+import { ko } from 'react-date-range/dist/locale';
+import { DateRange } from 'react-date-range';
+
+const Container = styled.div`
+
+`;
+
+const UserContainer = styled.div`
+
+`;
+
+const FormContainer = styled.div`
+
+`;
+
+const Calendar = styled.div`
+
+`;
+
+const Forms = styled.div`
+
+`;
+
+const Form = styled.div`
+
+`;
+
 
 export default ({})=>{
+        const [state, setState] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection',
+            // color: "#9E4040"
+        }
+    ]);
+
+    const forms=[
+        {startDate: new Date(2021, 7, 10),
+        endDate: new Date(2021, 7, 12)}
+    ]
 
     return (
-        <div>
-            Mypage
-        </div>
+        <Container>
+            <UserContainer>
+                user info
+            </UserContainer>
+            
+            <FormContainer>
+                <DateRange
+                    onChange={item => {}}
+                    moveRangeOnFirstSelection={false}
+                    ranges={state}
+                    locale={ko}
+                    direction="horizontal"
+                />
+                <Forms>
+                    <Form onClick = {()=>{setState(
+                    [{
+                                startDate: new Date(2021, 7, 10),
+                            endDate: new Date(2021, 7, 12),
+                                key: 'selection',
+                                // color: "#9E4040"
+                            }]
+                    )}}> 폼 </Form>
+                    <Form> 폼 </Form>
+                    <Form> 폼 </Form>
+                </Forms>
+
+            </FormContainer>
+        </Container>
     )
 }
