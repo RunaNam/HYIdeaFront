1f38ee3936f25d244f3e072c2fe3223fae6d214c
RunaNam <nmj5012@naver.com>
2021-06-28 15:37:53 +0900

    필요없는 코드 삭제

--- src/Routes/Submit/Calendar.js
+++ src/Routes/Submit/Calendar.js
@@ -7,41 +7,10 @@ function CalendarRow({ startDate, endDate }) {
     const ableClick = 0;
     let tmpDate = new Date(startDate);
 
-    // calendar 라는 변수에 넣어두고 그걸 useEffect에서 mycalendar로 넣음..
     let [calendar, setCalendar] = useState([]);
-    const [myCal, setMyCal] = useState([]);
     const [initial, setInital] = useState(true);
     let count = 0;
 
-
-    
-
-    // while (tmpDate <= endDate) {
-    //     const tmp = new Date(tmpDate);
-    //     calendar = [...calendar, { date: tmp, click: ableClick }];
-    //     tmpDate.setDate(tmpDate.getDate() + 1);
-    //     count++;
-    // }
-    // while (count % 7 != 0) {
-    //     calendar = [...calendar, { date: tmpDate, click: disableClick }];
-    //     count++;
-    // }
-
-    // let count = 0;
-    // while (count < startDay) {
-    //     calendar = [...calendar, { date: tmpDate, click: disableClick }];
-    //     count++;
-    // }
-    // while (tmpDate <= endDate) {
-    //     const tmp = new Date(tmpDate);
-    //     calendar = [...calendar, { date: tmp, click: ableClick }];
-    //     tmpDate.setDate(tmpDate.getDate() + 1);
-    //     count++;
-    // }
-    // while (count % 7 != 0) {
-    //     calendar = [...calendar, { date: tmpDate, click: disableClick }];
-    //     count++;
-    // }
 
     if(initial){
         while (count < startDay) {
@@ -62,41 +31,15 @@ function CalendarRow({ startDate, endDate }) {
     setCalendar(calendar)
     }
 
-    if (count < startDay) {
-        console.log(count)
-        calendar = [...calendar];
-        count++;
-    }
-
-    // // // 초기화 함수, 첫 렌더링때만 돌아감.
-    // useEffect(()=>{
-    //     if(initial){
-    //         let count = 0;
-    //         while (count < startDay) {
-    //             calendar = [...calendar, { date: tmpDate, click: disableClick }];
-    //             count++;
-    //         }
-    //         while (tmpDate <= endDate) {
-    //             const tmp = new Date(tmpDate);
-    //             calendar = [...calendar, { date: tmp, click: ableClick }];
-    //             tmpDate.setDate(tmpDate.getDate() + 1);
-    //             count++;
-    //         }
-    //         while (count % 7 != 0) {
-    //             calendar = [...calendar, { date: tmpDate, click: disableClick }];
-    //             count++;
-    //         }
-    //     }
-    //     setInital(false)
-    //     setCalendar(calendar)
-    // },[])
-
-
+    useEffect(()=>{
+            calendar = [...calendar]
+    })
 
     const handleClick=(key)=>{
         const newC = calendar;
         newC[key].click = newC[key].click +1;
         setCalendar(newC)
+        console.log(calendar)
     }
 
     return (
