2f2061710f39f3670607b4f8c219540d0b238890
Soonwoo Kwon <louis981220@gmail.com>
2021-06-28 13:37:28 +0900

    Canlendar.js 일부 수정

--- src/Routes/Submit/Calendar.js
+++ src/Routes/Submit/Calendar.js
@@ -14,7 +14,7 @@ function CalendarRow({ startDate, endDate }) {
     let count = 0;
 
 
-    
+
 
     // while (tmpDate <= endDate) {
     //     const tmp = new Date(tmpDate);
@@ -43,7 +43,7 @@ function CalendarRow({ startDate, endDate }) {
     //     count++;
     // }
 
-    if(initial){
+    if (initial) {
         while (count < startDay) {
             calendar = [...calendar, { date: tmpDate, click: disableClick }];
             count++;
@@ -58,8 +58,8 @@ function CalendarRow({ startDate, endDate }) {
             calendar = [...calendar, { date: tmpDate, click: disableClick }];
             count++;
         }
-    setInital(false)
-    setCalendar(calendar)
+        setInital(false)
+        setCalendar(calendar)
     }
 
     if (count < startDay) {
@@ -93,30 +93,30 @@ function CalendarRow({ startDate, endDate }) {
 
 
 
-    const handleClick=(key)=>{
+    const handleClick = (key) => {
         const newC = calendar;
-        newC[key].click = newC[key].click +1;
+        newC[key].click = (newC[key].click + 1) % 3;
         setCalendar(newC)
     }
 
     return (
         <div>{
             calendar.map((date, key) => {
-                    return (
+                return (
                     <>
                         {key % 7 === 0 && <div></div>}
                         {date.click === -1 ? (<button className="disable"></button>) : (
-                            <> 
-                                {date.click % 3 == 0 && <button className="possible" onClick={()=>{handleClick(key);}} > {date.date.getDate()} {date.click} </button>}
-                                {date.click % 3 == 1 && <button className="adjustable" onClick={()=>{handleClick(key);}}> {date.date.getDate()} {date.click}</button>}
-                                {date.click % 3 == 2 && <button className="impossible"onClick={()=>{handleClick(key);}}> {date.date.getDate()}{date.click} </button>}
+                            <>
+                                {date.click == 0 && <button className="possible" onClick={() => { handleClick(key); }} > {date.date.getDate()} {date.click} </button>}
+                                {date.click == 1 && <button className="adjustable" onClick={() => { handleClick(key); }}> {date.date.getDate()} {date.click}</button>}
+                                {date.click == 2 && <button className="impossible" onClick={() => { handleClick(key); }}> {date.date.getDate()} {date.click} </button>}
                             </>
                         )}
                     </>
-                    )
-                }
+                )
+            }
             )
-            }</div>
+        }</div>
     )
 }
 
