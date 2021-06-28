676691d8585f2815e01e8162fe58e6a809cc7d54
Soonwoo Kwon <louis981220@gmail.com>
2021-06-28 13:34:50 +0900

    Calendar.js

--- src/Routes/Submit/Calendar.js
+++ src/Routes/Submit/Calendar.js
@@ -14,11 +14,7 @@ function CalendarRow({ startDate, endDate }) {
     let count = 0;
 
 
-<<<<<<< HEAD
-
-=======
     
->>>>>>> bd65653bbdab68c19450409f2512ee93488277d5
 
     // while (tmpDate <= endDate) {
     //     const tmp = new Date(tmpDate);
@@ -47,11 +43,7 @@ function CalendarRow({ startDate, endDate }) {
     //     count++;
     // }
 
-<<<<<<< HEAD
-    if (initial) {
-=======
     if(initial){
->>>>>>> bd65653bbdab68c19450409f2512ee93488277d5
         while (count < startDay) {
             calendar = [...calendar, { date: tmpDate, click: disableClick }];
             count++;
@@ -66,13 +58,8 @@ function CalendarRow({ startDate, endDate }) {
             calendar = [...calendar, { date: tmpDate, click: disableClick }];
             count++;
         }
-<<<<<<< HEAD
-        setInital(false)
-        setCalendar(calendar)
-=======
     setInital(false)
     setCalendar(calendar)
->>>>>>> bd65653bbdab68c19450409f2512ee93488277d5
     }
 
     if (count < startDay) {
@@ -106,38 +93,15 @@ function CalendarRow({ startDate, endDate }) {
 
 
 
-<<<<<<< HEAD
-    const handleClick = (key) => {
-        const newC = calendar;
-        newC[key].click = (newC[key].click + 1) % 3;
-=======
     const handleClick=(key)=>{
         const newC = calendar;
         newC[key].click = newC[key].click +1;
->>>>>>> bd65653bbdab68c19450409f2512ee93488277d5
         setCalendar(newC)
     }
 
     return (
         <div>{
             calendar.map((date, key) => {
-<<<<<<< HEAD
-                return (
-                    <>
-                        {key % 7 === 0 && <div></div>}
-                        {date.click === -1 ? (<button className="disable"></button>) : (
-                            <>
-                                {date.click == 0 && <button className="possible" onClick={() => { handleClick(key); }} > {date.date.getDate()} {date.click} </button>}
-                                {date.click == 1 && <button className="adjustable" onClick={() => { handleClick(key); }}> {date.date.getDate()} {date.click}</button>}
-                                {date.click == 2 && <button className="impossible" onClick={() => { handleClick(key); }}> {date.date.getDate()} {date.click} </button>}
-                            </>
-                        )}
-                    </>
-                )
-            }
-            )
-        }</div>
-=======
                     return (
                     <>
                         {key % 7 === 0 && <div></div>}
@@ -153,7 +117,6 @@ function CalendarRow({ startDate, endDate }) {
                 }
             )
             }</div>
->>>>>>> bd65653bbdab68c19450409f2512ee93488277d5
     )
 }
 
